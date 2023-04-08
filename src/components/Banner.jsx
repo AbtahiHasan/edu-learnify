8688e5caae5835339d3b67243d145bc678f002f6
AbtahiHasan <anikidpersonal@gmail.com>
2023-04-08 13:53:53 +0600

    main menu responsive done

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
         <section className='relative'>
             <div className='before:content-[""] before:absolute before:blur-sm before:top-0 before:bottom-0 before:left-0 before:right-0 before:backdrop-blur-xl before:z-0 before:bg-cover before:bg-[url("https://htmldemo.net/glaxdu/glaxdu/assets/img/slider/slider-1.jpg")]  bg-cover  w-full h-screen'></div>
             <div className='absolute top-0 left-0 right-0 bottom-0 ui-container text-white flex flex-col justify-center'>
-                <div className='w-1/2'>
+                <div className='md:w-1/2'>
                     <h1 className='text-5xl font-bold'>Learn New</h1>
                     <p className='my-5'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor, assumenda alias neque cum illum ipsam repellat totam at adipisci doloribus? Nostrum reiciendis placeat veniam expedita ducimus quae dolorum, provident voluptatem.</p>
                     <div className='flex gap-5 mt-10'>
--- src/components/header/MainNav.jsx
+++ src/components/header/MainNav.jsx
@@ -1,25 +1,39 @@
 import logo from '/logo.svg'
 import { NavLink } from 'react-router-dom';
 import { AiOutlineShoppingCart, AiOutlineSearch } from "react-icons/ai";
+import { HiMenuAlt2 } from "react-icons/hi";
+import { MdClose } from "react-icons/md";
+import { useState } from 'react';
+
+
 
 
 const MainNav = () => {
+    const [toggle, setToggle] = useState(false)
     const navlinks = ["home", "about", "shop", "pages", "about us", "blog", "contact"]
     return (
         <section className='py-6 bg-white relative z-50'>
-            <div className='ui-container flex items-center justify-between'>
+            <div className='ui-container flex items-center justify-between relative'>
                 {/* logo  */}
                 <div className='flex gap-2 items-center'>
                     <img src={logo} alt="logo" />
                     <h2 className='text-2xl font-bold'>Edu-Learnify</h2>
                 </div>
-                <ul className='md:flex gap-4 items-center '>
+               
+                <ul className={`md:flex absolute transition-all md:static w-full md:w-auto gap-4 top-16 bg-black md:bg-transparent text-white md:text-black items-center ${toggle ? "left-0": "-left-full"} `}>
                     {
-                        navlinks.map((name, index) => <li key={index}><NavLink>{name.toUpperCase()}</NavLink></li>)
-                    }
-                    <li><button><AiOutlineShoppingCart className='font-bold text-xl'/></button></li>
-                    <li><button><AiOutlineSearch className='font-bold text-xl'/></button></li>
+                        navlinks.map((name, index) => <li className='p-3 text-center' key={index}><NavLink>{name.toUpperCase()}</NavLink></li>)
+                    }                  
                 </ul>
+                <div className='ml-auto'>
+                    <button><AiOutlineShoppingCart className='font-bold text-xl mr-2'/></button>
+                    <button><AiOutlineSearch className='font-bold text-xl mr-2'/></button>
+                </div>
+                <div className='md:hidden'>
+                {
+                    toggle ? <MdClose className='text-xl' onClick={() => setToggle(!toggle)}/> : <HiMenuAlt2 className='text-xl' onClick={() => setToggle(!toggle)}/>
+                }
+                </div>
             </div>
         </section>
     );
