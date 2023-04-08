faf5c98c8d34ba78ff2a5b846beda6493dcf5371
AbtahiHasan <anikidpersonal@gmail.com>
2023-04-08 13:55:46 +0600

    top navbar responsive done

--- src/components/header/TopNav.jsx
+++ src/components/header/TopNav.jsx
@@ -5,7 +5,7 @@ import { NavLink } from "react-router-dom";
 const TopNav = () => {
     return (
         <section className='bg-green-700 py-3'>
-            <div className='ui-container text-white flex justify-between items-center font-bold'>
+            <div className='ui-container text-white flex flex-col md:flex-row md:justify-between items-center font-bold'>
                 <div className="flex gap-5">
                     <p className="flex gap-2 items-center"> <BsFillTelephoneFill/> <span> +98 558 547 589 </span></p>
                     <p className="flex gap-2 items-center"> <TfiEmail/> <span> mail@edu-learnify.com </span></p>
